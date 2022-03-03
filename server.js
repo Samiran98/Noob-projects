cdced635092062cee38492550ff920e0a8ad32a5
Samiran Ghosh <samiran.ghosh@codelogicx.com>
2022-03-03 20:36:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/article')
+const Article = require('./models/article')
+const methodOverride = require('method-override')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog')
+.then(() => console.log("Database connected!"))
+ .catch(err => console.log(err))
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended : false }))
+app.use(methodOverride('_method'))
+
+app.get('/',async (req, res) => {
+    const articles = await Article.find()
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
